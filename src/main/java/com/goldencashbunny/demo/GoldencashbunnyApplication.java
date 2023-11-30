c6ee38cd9731a31ce640c95a7c4f81a9417f7f8c
erico cavalcanti <ecocaval@hotmail.com>
2023-11-30 12:23:18 -0300

    first commit

---
+++ src/main/java/com/goldencashbunny/demo/GoldencashbunnyApplication.java
@@ -0,0 +1,13 @@
+package com.goldencashbunny.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoldencashbunnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoldencashbunnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goldencashbunny/demo/GoldencashbunnyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goldencashbunny.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldencashbunnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
